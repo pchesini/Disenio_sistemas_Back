46f7f40c7cc0bde5ec354e853b91c96a7e9916c9
Lilith Nix <andromeda0209@gmail.com>
2023-12-03 23:09:48 -0300

    agregué crossOrigin a ComunicacionCientificaController

--- disenio/src/main/java/Controlador/ComunicacionCientificaController.java
+++ disenio/src/main/java/Controlador/ComunicacionCientificaController.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:4200" )
 @RestController
 @RequestMapping("comunicacionCientifica")
 public class ComunicacionCientificaController {
